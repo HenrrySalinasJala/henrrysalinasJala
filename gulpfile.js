c4bf8559813a55d47e553dff4f2de720c0c17279
henrry.salinas <henrry.salinas@avantica.com>
2022-03-15 00:02:02 -0400

    adding eslint rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,12 @@
-//const { src, dest } = require("gulp");
 const gulp = require('gulp');
 const testcafe = require('gulp-testcafe');
 const fs = require('fs');
 
-gulp.task('default', () => {
-  return gulp.src('src/tests/**/*.js')
-    .pipe(testcafe({ browsers: ['chrome', 'firefox'] }));
-});
+gulp.task('default', () => gulp.src('src/tests/**/*.js')
+  .pipe(testcafe({ browsers: ['chrome', 'firefox'] })));
 
-gulp.task('burger', () => {
-  return gulp.src('src/tests/burgerbuilderTest.js')
-    .pipe(testcafe({ browsers: ['chrome'], reporter: [{ "name": "spec" }, { "name": "json", "output": "reports/report.json" }] }));
-});
+gulp.task('burger', () => gulp.src('src/tests/burgerbuilderTest.js')
+  .pipe(testcafe({ browsers: ['chrome'], reporter: [{ name: 'spec' }, { name: 'json', output: 'reports/report.json' }] })));
 
-
-gulp.task('todo', () => {
-  return gulp.src('src/tests/todo.js')
-    .pipe(testcafe({ browsers: ['firefox'], reporter: [{ "name": "spec" }, { "name": "xunit", "output": fs.createWriteStream("reports/report.xml") }] }));
-});
+gulp.task('todo', () => gulp.src('src/tests/todo.js')
+  .pipe(testcafe({ browsers: ['firefox'], reporter: [{ name: 'spec' }] })));
