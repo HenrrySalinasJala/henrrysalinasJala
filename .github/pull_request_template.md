ed21a24145394618cd3e7ac4a1615b36ea59dbb6
Henrry Salinas <henrry.salinas@bairesdev.com>
2022-06-14 21:39:09 -0400

    adding prettier rules for linting checks

---
+++ .github/pull_request_template.md
@@ -0,0 +1,23 @@
+# Description
+
+<!-- Please include a summary of the change and which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change. -->
+
+## Jira Ticket Link
+
+<!-- Please include the related tickets on Jira -->
+
+## TestRail cases Links
+
+<!-- Please include the related test cases on Test Rail -->
+
+## Test Env:
+
+<!-- Please specify the environments that these implementation was run against-->
+
+- [] QA env
+- [] Staging env
+- [] Production
+
+## CI:
+
+<!-- Please include evidence of testing being done on CI tool links or screenshots-->
--- src/main/actions/get_started.actions.js
+++ src/main/actions/get_started.actions.js
@@ -1,8 +1,8 @@
-import { t as actions, t } from 'testcafe'
+import { t as actions } from 'testcafe'
 import faker from 'faker'
 import sample from 'lodash/sample'
-import GetStartedPage from '../pages/get_started_page'
-import SelfRegistrationModal from '../pages/self_registration_modal'
+import GetStartedPage from '../pages/get_started.page'
+import SelfRegistrationModal from '../pages/self_registration.modal.page'
 
 /**
  * @class HomePageActions
@@ -38,23 +38,19 @@ export default class HomePageActions {
     const profileDropDown = this.selfRegistrationModal.getWhoAreYouDroDown()
     const profile = sample(['Traveler'])
     const optionElement = this.selfRegistrationModal.getDropDownOption(profileDropDown, profile)
-    await actions
-      .click(profileDropDown)
-      .click(optionElement)
+    await actions.click(profileDropDown).click(optionElement)
     const countryCode = `+${faker.datatype.number({ min: 590, max: 599 })}`
     await actions.typeText(this.selfRegistrationModal.getCountryCode(), countryCode)
     await actions.typeText(
       this.selfRegistrationModal.getContactPhone(),
-      faker.datatype.number({ min: 60000000, max: 90000000 }).toString(),
+      faker.datatype.number({ min: 60000000, max: 90000000 }).toString()
     )
     await actions.typeText(this.selfRegistrationModal.getBusinessAddress(), faker.address.streetAddress())
     await actions.typeText(this.selfRegistrationModal.getAddress2(), faker.address.secondaryAddress())
     const randomCountry = sample(['Bolivia', 'Paraguay'])
     const countryDropDown = this.selfRegistrationModal.getCountry()
     const countryElement = this.selfRegistrationModal.getDropDownOption(countryDropDown, randomCountry)
-    await actions
-      .click(countryDropDown)
-      .click(countryElement)
+    await actions.click(countryDropDown).click(countryElement)
     await actions.typeText(this.selfRegistrationModal.getState(), faker.address.state())
     await actions.typeText(this.selfRegistrationModal.getCity(), faker.address.city())
     await actions.typeText(this.selfRegistrationModal.getZip(), '0000')
